5417ff29962ef4d130e9bbe5f9956ffacbd30e20
Corentin Rodrigo <corentin.rodrigo@2019.icam.fr>
2026-01-25 17:44:39 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { RouterLink } from '@angular/router'
+import { CommonModule, NgOptimizedImage } from '@angular/common'
 
 @Component({
-  selector: 'app-about',
-  imports: [],
-  templateUrl: './about.html',
-  styleUrl: './about.scss',
+	selector: 'app-about',
+	imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule, RouterLink, NgOptimizedImage],
+	templateUrl: './about.html',
+	styleUrl: './about.scss',
 })
-export class About {
-
-}
+export class About {}
