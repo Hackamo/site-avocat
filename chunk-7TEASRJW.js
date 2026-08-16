021d6d5de22cf4504d91e2d4fcc1e908e85aae50
Corentin Rodrigo <corentin.rodrigo@2019.icam.fr>
2026-08-16 20:06:08 +0200

    Auto-generated commit

---
+++ chunk-7TEASRJW.js
@@ -0,0 +1,1 @@
+import{Ma as e,S as i,ud as r}from"./chunk-65I3OEYF.js";var c=(()=>{class t{static \u0275fac=function(n){return new(n||t)};static \u0275mod=e({type:t});static \u0275inj=i({imports:[r]})}return t})();export{c as a};
